e28d2231b1ad72b8b06a0a6f1be42ceeef58ab67
Moktadir Rahman <autocircled@gmail.com>
2024-01-02 01:09:25 +0600

    contact page breadcrumb added

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout"
+import BreadcrumbSection from "../components/common/BreadcrumbSection"
 import ContactForm from "../components/contact/ContactForm"
 import Map from "../components/contact/Map"
 
 const ContactPage = () => {
 	return (
 		<Layout>
+			<BreadcrumbSection title="Contact us" list={["Home", "Contact"]} />
 			<ContactForm />
 			<Map />
 		</Layout>
